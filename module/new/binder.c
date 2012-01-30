9aa558830982c51d898a3817a03e11c1897920c6
Rong Shen <rong1129@gmail.com>
2012-01-30 12:38:15 +1100

    seperated instrumentation code

--- module/new/binder.c
+++ module/new/binder.c
@@ -34,52 +34,7 @@
 
 #include "msg_queue.h"
 #include "binder.h"
-
-
-#define KERNEL_INSTRUMENTING
-#ifdef KERNEL_INSTRUMENTING
-typedef union {
-	struct timeval tv;
-	char label[8];
-} inst_entry_t;
-
-typedef struct {
-	uint32_t magic;
-	uint32_t seq;
-	uint32_t max_entries;
-	uint32_t next_entry;
-	inst_entry_t entries[0];
-} inst_buf_t;
-
-static inline void __inst_entry(void *ptr, char *label, struct timeval *copy)
-{
-	inst_buf_t *inst = (inst_buf_t *)ptr;
-
-	if (inst->magic != 0x696e7374)
-		return;
-
-	if (inst->next_entry < inst->max_entries) {
-		inst_entry_t *entry = inst->entries + inst->next_entry++;
-
-		if (inst->seq) {
-			if (!copy) 
-				 do_gettimeofday(&entry->tv);
-			else
-				entry->tv = *copy;
-		} else {
-			strncpy(entry->label, label, 8);
-			entry->label[7] = '\0';
-		}
-	}
-}
-#define INST_ENTRY(p, label)			__inst_entry((p), (label), NULL)
-#define INST_ENTRY_COPY(t, p, label, n)		__inst_entry((p), (label), (t)->__inst_copies + (n))
-#define INST_RECORD(t, n)			do_gettimeofday((t)->__inst_copies + (n));
-#else
-#define INST_ENTRY(b, label)
-#define INST_ENTRY_COPY(t, p, label, n)
-#define INST_RECORD(t, n)
-#endif /* KERNEL_INSTRUMENTING */
+#include "inst.h"
 
 
 #define MAX_TRANSACTION_SIZE			4000
@@ -660,7 +615,7 @@ static int binder_free_obj(struct binder_proc *proc, struct binder_obj *obj)
 			list_del(&notifier->list);
 
 			if (!msg) {
-				msg = kmalloc(sizeof(*msg), GFP_KERNEL); // TODO: ugly
+				msg = binder_alloc_msg(0, 0); // TODO: ugly
 				if (!msg) {
 					kfree(obj);
 					return -ENOMEM;
@@ -900,12 +855,11 @@ static int bcmd_write_transaction(struct binder_proc *proc, struct binder_thread
 			err = BR_FAILED_REPLY;
 			goto failed_load;
 		}
-
-		INST_ENTRY_COPY(thread, msg->buf->data, "K_IOC", 0);
-		INST_ENTRY_COPY(thread, msg->buf->data, "K_WR_IN", 1);
-		INST_ENTRY(msg->buf->data, "K_ENQ");
 	}
 
+	INST_ENTRY_COPY(thread, msg->buf->data, "K_IOC", 0);
+	//INST_ENTRY_COPY(thread, msg->buf->data, "K_WR_IN", 1);
+	INST_ENTRY(msg->buf->data, "K_ENQ_IN");
 	if (bcmd_write_msg(q, msg) < 0) {
 		err = BR_DEAD_REPLY;
 		goto failed_write;
